8894922df38e4171c86751e6629e241d8267c37b
frarm <frarm2014@outlook.com>
2022-12-19 19:07:58 -0500

    Logging

---
+++ src/main/java/com/domo/demo/login/LoginController.java
@@ -0,0 +1,24 @@
+package com.domo.demo.login;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    
+    //http://localhost:8080/login?name=franco
+    @RequestMapping("login")
+    public String gotologinPage(@RequestParam String name, ModelMap model){
+        model.put("name", name);
+        logger.debug("Request param is {}", name);
+        logger.info("Info Level");
+        logger.warn("Warm Level");
+        return "login";
+    }
+}
